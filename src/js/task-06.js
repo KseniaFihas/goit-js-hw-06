972bb5e5804aa279a118e83baeb98aecdf983acc
Ksenia Fihas <ksenia.fihas@gmail.com>
2023-02-09 19:55:50 +0100

    Update task-06.js

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -3,7 +3,7 @@ const input = document.querySelector("#validation-input");
     
     input.addEventListener('blur', event => {
   
-    if (event.target.value.length === input.getAttribute('data-length')) {
+    if (event.target.value.length == input.getAttribute('data-length')) {
         input.classList.add('valid');
       
       
