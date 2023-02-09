6e28a7bc61ec44d4501b8bd323a9e9429d4b3adf
Ksenia Fihas <ksenia.fihas@gmail.com>
2023-02-09 12:47:55 +0100

    Update task-09.js

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,22 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+
+
+
+
+const button = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+const body = document.querySelector("body");
+
+button.addEventListener('click', changeColor => {
+  const backGroundColor = getRandomHexColor();
+
+  body.style.backgroundColor = backGroundColor;
+ 
+
+});
+
+
+
