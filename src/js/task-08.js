256f509b3e06419fc13d5b4c9fbec2165074299c
Ksenia Fihas <ksenia.fihas@gmail.com>
2023-02-09 12:27:40 +0100

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener('submit', confirm);
+
+
+function confirm(event) {
+    event.preventDefault();
+
+    const {
+        elements: { email, password },
+    } = event.currentTarget;
+
+
+    if (email.value === "" || password.value === "") {
+        return alert("Всі поля повинні бути заповнені!")
+    }
+
+    const user = { email: email.value, password: password.value }
+    
+    console.log(user);
+
+    event.currentTarget.reset();
+}
