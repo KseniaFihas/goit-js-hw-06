2c02181e307568e895319f26301a94317bc82e89
Ksenia Fihas <ksenia.fihas@gmail.com>
2023-02-09 12:05:35 +0100

    task 7

--- src/js/task-06.js
+++ src/js/task-06.js
@@ -1,15 +1,19 @@
 const input = document.querySelector("#validation-input");
-
-
-input.addEventListener('blur', event => {
-    if (event.target.value.length == input.getAttribute("data-length")) {
-      
-        input.classList.add("valid");
-
-    } else {
-        
-        input.classList.add("invalid");
-    }
     
+    
+    input.addEventListener('blur', event => {
+  
+    if (event.target.value.length === input.getAttribute('data-length')) {
+        input.classList.add('valid');
+      
+      
+        if (input.classList.contains('invalid')){
+            input.classList.remove('invalid');
+        }
+    } else {
+        if (input.classList.contains('valid')) {
+            input.classList.remove('valid');
+        }
+        input.classList.add('invalid')
+    }
 });
-
